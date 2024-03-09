fb08e7e35d3dddaee00991199f3c1901c7a1261f
Perry Hertler <perry@hertler.org>
2024-03-09 15:10:29 -0600

    starting over

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,26 @@
-use std::env;
 use std::fs;
-use std::io::Read;
-use anyhow::Context;
-use flate2::read::ZlibDecoder;
+use clap::command;
+use clap::Parser;
+use clap::Subcommand;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    command: Command,
+}
+#[derive(Subcommand, Debug)]
+#[command(about, long_about = None)]
+enum Command {
+    Init,
+}
+
+fn main() {
+ let args = Args::parse();
+    match args.command {
+        Command::Init => init(),
+    }
+}
 
 fn init() {
     fs::create_dir(".git").unwrap();
@@ -12,41 +30,13 @@ fn init() {
     println!("Initialized git directory")
 }
 
-const OBJECT_DIR: &str = ".git/objects/";
-
-fn cat_file(_flag: &str, sha: &str) -> anyhow::Result<()> {
-    let path = format!("{}{}/{}", OBJECT_DIR, &sha[..2], &sha[2..]);
-    let content = fs::read(path).context("failed to read object")?;
-    let mut z = ZlibDecoder::new(&content[..]);
-    let mut s = String::new();
-    z.read_to_string(&mut s).unwrap();
-    print!("{}", &s[8..]);
-
-    Ok(())
-}
-
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    match &*args[1] {
-        "init" => init(),
-        "cat-file" => cat_file(&args[2], &args[3]).expect("failed to cat file"),
-        _ => println!("unknown command: {}", args[1]),
-    }
-}
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::fs::File;
-    use std::io::prelude::*;
 
     #[test]
     fn test_cat_file() {
-        let mut file = File::create("test.txt").unwrap();
-        file.write_all(b"test").unwrap();
-        let mut file = File::open("test.txt").unwrap();
-        let mut contents = String::new();
-        file.read_to_string(&mut contents).unwrap();
-        assert_eq!(contents, "test");
+       
     }
 }
